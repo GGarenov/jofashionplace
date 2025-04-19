7b9c2f0dcb1312d8d13e821cbf39c6a01113676f
Georgi <g_garenov@outlook.com>
2025-04-19 21:01:55 +0300

    tests are implemented

--- server/server.js
+++ server/server.js
@@ -47,6 +47,10 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in development mode on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () =>
+    console.log(`Server running in development mode on port ${PORT}`)
+  );
+}
+
+module.exports = app;
